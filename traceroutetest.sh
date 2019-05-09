01221b4f0f720ec15b24546514241411ade959f9
Benjamin Cornwell <33430452+bcornw2@users.noreply.github.com>
2019-05-09 19:54:37 -0400

    Update traceroutetest.sh

--- traceroutetest.sh
+++ traceroutetest.sh
@@ -1,8 +1,10 @@
 #the following traceroutetest.sh is run under the following conditions:
-#   watch -n 60 traceroutetest.sh
+#   watch -n1 traceroutetest.sh  
 #   repeats the timestamped traceroute and appends the output and date to the end of a text file, and shows in console
 
 #script traceroute.sh
 
-date + "%d-%m-%y" 2>&1 | tee tracerouteout.txt
-Traceroute horwitzstudio.com 2>&1 | tee tracerouteout.txt
+date + "%d-%m-%y   %T" 2>&1 | tee --append tracerouteout.txt
+Traceroute horwitzstudio.com 2>&1 | tee --append tracerouteout.txt
+
+#initializes each entry with date and time, then outputs a full traceroute, and each subsequent entry is appended to end of list instead of overwritten
